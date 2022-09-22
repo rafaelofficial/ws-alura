92d430cfb1a804f1244e99c0114dd9245b7ab39a
rafaelofficial <rafael.sousa.pereira@mail.usf.edu.br>
2022-09-22 18:10:55 -0300

    feat: functions

---
+++ javascript/functions/functions.js
@@ -0,0 +1,14 @@
+// 1 - Function declarate
+function printAnyText(text) {
+    console.log(text);
+}
+
+// 2 - Function execute (1x or 2x)
+printAnyText("Hello world");
+
+// Three form of write function
+function sum(x, y) {
+    return x + y;
+}
+
+printAnyText(sum(2, 2));
