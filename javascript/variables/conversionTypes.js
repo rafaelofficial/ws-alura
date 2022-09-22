52b2d5d24515f2ad07d3b749c2a0bb442ee7e149
rafaelofficial <rafael.sousa.pereira@mail.usf.edu.br>
2022-09-22 15:04:07 -0300

    more about types

--- javascript/variables/conversionTypes.js
+++ javascript/variables/conversionTypes.js
@@ -10,3 +10,6 @@ const numeroString = Number("450");
 console.log(numero + numeroString);
 
 // conversão explicita
+let telefone = 12341234;
+// teremos a conversão do número 12341234 para uma string “12341234” e assim poderemos fazer a concatenação entre as strings
+console.log("O telefone é " + String(telefone));
