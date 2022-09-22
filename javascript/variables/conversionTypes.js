79e00314b445599a88abec552c1085e5e57bcf3b
rafaelofficial <rafael.sousa.pereira@mail.usf.edu.br>
2022-09-22 14:52:32 -0300

    more about types

---
+++ javascript/variables/conversionTypes.js
@@ -0,0 +1,12 @@
+// type data
+// booleans
+
+// conversão implícita
+const numero = 450;
+const numeroString = Number("450");
+
+// Number()
+// String()
+console.log(numero + numeroString);
+
+// conversão explicita
---
+++ javascript/variables/truthy_falsy.js
@@ -0,0 +1,21 @@
+// boolean
+const userLoged = true;
+const accountPaid = false;
+
+// truthy or falsy
+
+console.log(0 == false);
+console.log("" == false);
+console.log(1 == true);
+
+let myVar;
+let varNull = null;
+
+console.log(typeof myVar);
+console.log(typeof varNull);
+
+let number = 3;
+let text = "Alura";
+
+console.log(typeof number);
+console.log(typeof text);
